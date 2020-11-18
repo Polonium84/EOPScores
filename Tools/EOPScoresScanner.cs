245bd28edd430cf7fafe70f8b763757926f80167
Polonium <63310093+Polonium84@users.noreply.github.com>
2020-11-18 23:34:52 +0800

    An tool to build a SQLite database

---
+++ Tools/EOPScoresScanner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+using Microsoft.Data.Sqlite;
+
+namespace EOPScoresScanner
+{
+    class EOPScoresScanner
+    {
+        static void Main(string[] args)
+        {
+            string RootDir = @"F:\EOPScores";
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class _EOP_CLASS
+    {
+        static string[] _Classes =
+        {
+            "流行",
+            "影视",
+            "经典",
+            "动漫",
+            "儿歌",
+            "练习曲",
+            "轻音乐",
+            "原创",
+            "民乐",
+            "其他"
+        };
+        static bool _Contains(string name)
+        {
+            foreach (string s in _Classes)
+                if (s == name)
+                    return true;
+            return false;
+        }
+    }
+}
